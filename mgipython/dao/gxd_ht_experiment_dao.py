94bfcf4375e36ebe5dd1a35dda2056246aa4027f
Olin Blodgett <nilo@nilosplace.net>
2016-09-13 08:50:52 -0400

    Added fix for case insensitivity

--- mgipython/dao/gxd_ht_experiment_dao.py
+++ mgipython/dao/gxd_ht_experiment_dao.py
@@ -59,13 +59,13 @@ class GxdHTExperimentDAO(BaseDAO):
             query = query.filter(GxdHTExperiment._curationstate_key == int(curationstate_key))
 
         if search_query.has_valid_param("primaryid"):
-            primaryid = search_query.get_value("primaryid")
-            query = query.filter(GxdHTExperiment.primaryid.like(primaryid))
+            primaryid = search_query.get_value("primaryid").lower()
+            query = query.filter(db.func.lower(GxdHTExperiment.primaryid).like(primaryid))
 
         if search_query.has_valid_param("secondaryid"):
             accession = db.aliased(Accession)
-            secondaryid = search_query.get_value("secondaryid")
-            query = query.join(accession, GxdHTExperiment.secondaryid_objects).filter(accession.accid.like(secondaryid))
+            secondaryid = search_query.get_value("secondaryid").lower()
+            query = query.join(accession, GxdHTExperiment.secondaryid_objects).filter(db.func.lower(accession.accid).like(secondaryid))
 
         if search_query.has_valid_param("evaluatedby_object"):
             user = db.aliased(MGIUser)
